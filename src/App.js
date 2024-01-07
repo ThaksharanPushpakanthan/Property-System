ebe0bc3f3747e56a53e0d618282673fc73b780b8
Thaksharan <thaksharan@villvay.com>
2024-01-07 20:29:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React, { useState } from 'react';
 import SearchForm from './SearchForm';
-import properties from './properties.json'
+import propertiesData from './properties.json'
 
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
